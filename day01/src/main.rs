f46bfe0f6e282881c138ec04bc99bd67e241e4b6
ltabis <tab.rysak70@gmail.com>
2021-12-01 09:14:04 +0100

    :sparkles: finised day01 ex01. (it's terrible)

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = std::fs::read_to_string("./src/input.txt")?
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+
+    println!("input: {:?}", input);
+
+    let mut iterator = input.into_iter().peekable();
+    let mut sweep = 0;
+    while iterator.peek().is_some() {
+        let next = iterator.next().unwrap_or(0);
+        let peek = iterator.peek().unwrap_or(&0).clone();
+
+        println!("peek: {}, next: {}", peek, next);
+
+        sweep += [0, 1][(peek > next) as usize];
+    }
+
+    println!("number of sweeps: {}", sweep);
+
+    Ok(())
 }
