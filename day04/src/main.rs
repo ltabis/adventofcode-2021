624d414c5d96118a56b1bbcbc2bd7ed96a8d968d
ltabis <tab.rysak70@gmail.com>
2021-12-05 14:59:04 +0100

    :sparkles: created a simple parsing function for day04.

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,11 +4,49 @@ fn main() {
     ()
 }
 
-fn load_input() -> Result<Vec<String>, Box<dyn Error>> {
-    Ok(std::fs::read_to_string("./src/input.txt")?
-        .lines()
-        .map(|s| s.to_string())
-        .collect())
+type Pile = Vec<u64>;
+type Board = [[u64; 5]; 5];
+
+fn load_board_line(line: &str) -> [u64; 5] {
+    match line
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>()[..]
+    {
+        [a, b, c, d, e] => [a, b, c, d, e],
+        _ => panic!("board line isn't 5 number long."),
+    }
+}
+
+fn load_input() -> Result<(Pile, Vec<Board>), Box<dyn Error>> {
+    let input = std::fs::read_to_string("./src/input.txt")?;
+    let mut input = input.lines();
+
+    let pile = input
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|n| n.parse().unwrap())
+        .collect::<Vec<u64>>();
+
+    let mut boards = Vec::new();
+
+    loop {
+        // whitespace
+        if let None = input.next() {
+            break;
+        }
+
+        let row1 = load_board_line(input.next().unwrap());
+        let row2 = load_board_line(input.next().unwrap());
+        let row3 = load_board_line(input.next().unwrap());
+        let row4 = load_board_line(input.next().unwrap());
+        let row5 = load_board_line(input.next().unwrap());
+
+        boards.push([row1, row2, row3, row4, row5]);
+    }
+
+    Ok((pile, boards))
 }
 
 fn ex01(input: &Vec<String>) {}
