df19a01397025bb7e178c094c192fc7b89a6383a
ltabis <tab.rysak70@gmail.com>
2021-12-07 09:29:22 +0100

    :ambulance: fixed algorithm.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -9,14 +9,45 @@ fn load_data(src: &str) -> std::io::Result<Vec<usize>> {
         .collect())
 }
 
+fn simulate(days: usize, mut data: Vec<usize>) -> Vec<usize> {
+    println!("Initial state: {:?}", data);
+
+    for day in 0..days {
+        let mut spawn: Vec<usize> = Vec::new();
+
+        data.iter_mut().for_each(|fish| {
+            if *fish == 0 {
+                *fish = 6;
+                spawn.push(8);
+            } else {
+                *fish -= 1;
+            }
+        });
+
+        data.extend(spawn);
+        println!("After {} day(s): {:?}", day + 1, data);
+    }
+
+    data
+}
+
 #[cfg(test)]
 mod test {
-    use crate::load_data;
+    use crate::{load_data, simulate};
 
     #[test]
     fn test_example() {
         let data = load_data("./src/example.txt").unwrap();
 
         println!("data: {:?}", data);
+    }
+
+    #[test]
+    fn test_ex01() {
+        let data1 = load_data("./src/example.txt").unwrap();
+        let data2 = data1.clone();
+
+        assert_eq!(simulate(18, data1).len(), 26);
+        assert_eq!(simulate(80, data2).len(), 5934);
     }
 }
