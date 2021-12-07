d3308819ef5d81539267a1262469f4b695017402
ltabis <tab.rysak70@gmail.com>
2021-12-07 13:32:09 +0100

    :zap: optimized the algorithm a little bit.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,53 +1,81 @@
 fn main() {
-    println!("Hello, world!");
+    let data = load_data("./src/input.txt").unwrap();
+    println!(
+        "ex02 result: {}",
+        Simulation::new().simulate(256, data).len()
+    );
 }
 
-fn load_data(src: &str) -> std::io::Result<Vec<usize>> {
+fn load_data(src: &str) -> std::io::Result<Vec<u8>> {
     Ok(std::fs::read_to_string(src)?
         .split(",")
         .map(|n| n.parse().unwrap())
         .collect())
 }
 
-fn simulate(days: usize, mut data: Vec<usize>) -> Vec<usize> {
-    println!("Initial state: {:?}", data);
+struct Simulation(bool);
 
-    for day in 0..days {
-        let mut spawn: Vec<usize> = Vec::new();
-
-        data.iter_mut().for_each(|fish| {
-            if *fish == 0 {
-                *fish = 6;
-                spawn.push(8);
-            } else {
-                *fish -= 1;
-            }
-        });
-
-        data.extend(spawn);
-        println!("After {} day(s): {:?}", day + 1, data);
+impl Simulation {
+    pub fn new() -> Self {
+        Self { 0: false }
     }
 
-    data
+    pub fn debug(mut self) -> Self {
+        self.0 = true;
+        self
+    }
+
+    fn simulate(&self, days: usize, mut data: Vec<u8>) -> Vec<u8> {
+        if self.0 {
+            println!("Initial state: {:?}", data);
+        }
+
+        for day in 0..days {
+            let mut spawn: Vec<u8> =
+                Vec::with_capacity(data.iter().fold(0, |acc, time| acc + (*time == 0) as usize));
+
+            data.iter_mut().for_each(|fish| {
+                if *fish == 0 {
+                    *fish = 6;
+                    spawn.push(8);
+                } else {
+                    *fish -= 1;
+                }
+            });
+
+            data.extend(spawn);
+
+            if self.0 {
+                println!("After {} day(s): {:?}", day + 1, data);
+            }
+        }
+
+        data
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{load_data, simulate};
+    use crate::{load_data, Simulation};
 
     #[test]
     fn test_example() {
-        let data = load_data("./src/example.txt").unwrap();
+        let data1 = load_data("./src/example.txt").unwrap();
+        let data2 = data1.clone();
+        let data3 = data1.clone();
 
-        println!("data: {:?}", data);
+        assert_eq!(Simulation::new().debug().simulate(18, data1).len(), 26);
+        assert_eq!(Simulation::new().simulate(80, data2).len(), 5934);
+        assert_eq!(Simulation::new().simulate(256, data3).len(), 26984457539);
     }
 
     #[test]
     fn test_ex01() {
-        let data1 = load_data("./src/example.txt").unwrap();
-        let data2 = data1.clone();
+        let data = load_data("./src/input.txt").unwrap();
 
-        assert_eq!(simulate(18, data1).len(), 26);
-        assert_eq!(simulate(80, data2).len(), 5934);
+        println!(
+            "ex01 result: {}",
+            Simulation::new().simulate(80, data).len()
+        );
     }
 }
