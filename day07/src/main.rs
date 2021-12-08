f57304ad56529ac91e36ca466f2b91b6fd41565a
ltabis <tab.rysak70@gmail.com>
2021-12-08 20:41:58 +0100

    :sparkles: finished day07 ex01.

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    ()
+    let input = load_input("./src/input.txt").unwrap();
+
+    println!("ex01: {}", find_lowest_fuel_consumption(&input));
 }
 
 fn load_input(src: &str) -> std::io::Result<Vec<i64>> {
@@ -16,17 +18,50 @@ fn align_from_number(crabs: &Vec<i64>, position: i64) -> usize {
     })
 }
 
+fn find_average(crabs: &Vec<i64>) -> usize {
+    crabs.iter().sum::<i64>() as usize / crabs.len()
+}
+
+fn find_lowest_fuel_consumption(crabs: &Vec<i64>) -> usize {
+    let average = find_average(crabs);
+    let mut lowest_fuel_consumption = align_from_number(crabs, average as i64);
+
+    for i in (0..average).rev() {
+        let concumption = align_from_number(crabs, i as i64);
+        if concumption < lowest_fuel_consumption {
+            lowest_fuel_consumption = concumption;
+        }
+    }
+
+    let mut index = average;
+
+    loop {
+        let concumption = align_from_number(crabs, index as i64);
+        if concumption < lowest_fuel_consumption {
+            lowest_fuel_consumption = concumption;
+        } else {
+            break;
+        }
+        index += 1;
+    }
+
+    lowest_fuel_consumption
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{align_from_number, load_input};
+    use crate::{align_from_number, find_average, find_lowest_fuel_consumption, load_input};
 
     #[test]
     fn test_example() {
         let input = load_input("./src/test.txt").unwrap();
 
+        assert_eq!(find_average(&input), 4);
         assert_eq!(align_from_number(&input, 2), 37);
         assert_eq!(align_from_number(&input, 1), 41);
         assert_eq!(align_from_number(&input, 3), 39);
         assert_eq!(align_from_number(&input, 10), 71);
+
+        assert_eq!(find_lowest_fuel_consumption(&input), 37);
     }
 }
