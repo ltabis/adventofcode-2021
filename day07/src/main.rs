ce42a52664264178dfbe5bcec4d8f0acff8830ed
ltabis <tab.rysak70@gmail.com>
2021-12-08 09:38:41 +0100

    :sparkles: created a simple function to load data and to compute alignment from a given pos.

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
     ()
 }
+
+fn load_input(src: &str) -> std::io::Result<Vec<i64>> {
+    Ok(std::fs::read_to_string(src)?
+        .split(",")
+        .map(|n| n.parse().unwrap())
+        .collect())
+}
+
+/// align crabs horizontaly and returns the total fuel costs.
+fn align_from_number(crabs: &Vec<i64>, position: i64) -> usize {
+    crabs.iter().fold(0, |acc, crab_pos| {
+        acc + (position - crab_pos).abs() as usize
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{align_from_number, load_input};
+
+    #[test]
+    fn test_example() {
+        let input = load_input("./src/test.txt").unwrap();
+
+        assert_eq!(align_from_number(&input, 2), 37);
+        assert_eq!(align_from_number(&input, 1), 41);
+        assert_eq!(align_from_number(&input, 3), 39);
+        assert_eq!(align_from_number(&input, 10), 71);
+    }
+}
